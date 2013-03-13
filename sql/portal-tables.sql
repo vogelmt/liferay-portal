4049c87fa992643bd25d73ba2f3f4ee584a755ef
Shinn Lok <shinn.lok@liferay.com>
2013-03-13 13:00:31 -0700

    LPS-33085 Regenerate

--- sql/portal-tables.sql
+++ sql/portal-tables.sql
@@ -1145,7 +1145,14 @@ create table MBCategory (
 );
 
 create table MBDiscussion (
+	uuid_ VARCHAR(75) null,
 	discussionId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
 	classNameId LONG,
 	classPK LONG,
 	threadId LONG
